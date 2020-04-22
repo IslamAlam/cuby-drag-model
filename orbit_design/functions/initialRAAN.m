631d86c93ddd9236c4d852f3007c3634d391b488
ge29pit <max.helleis@tum.de>
2020-04-22 13:14:45 +0200

    Changed initialRAAN function to work for 2022

--- orbit_design/functions/initialRAAN.m
+++ orbit_design/functions/initialRAAN.m
@@ -1,23 +1,33 @@
-function jd = initialRAAN(initLOAN)
+function jd = initialRAAN(init_LOAN)
 %initialRAAN Computes the reference Julian Date so that a satellite with
 % a specified initial LOAN has a Local Mean Time of the Ascending Node. The
 % RAAN must be 330 ° if using this date. 
 %   Arguments
 %       initLOAN    -- initial longitude of the ascending node [decimal °]
+%   Variables / Constants
+%       ET          -- value of the Equation of time (20.03.2022) [min.]
+%       dt_ve_loan  -- time difference between initial LOAN and Greenwich
+%                      [hour angles]
+%       LAT_AN      -- equired Local Apparent Time at ascending node
+%                      [decimal hours] 
+%       LMT_AN      -- Local Mean Solar Time at ascending node
+%                      [decimal hours] (LMT ~ UTC @ 0° longitude)
+%       dt_ve_loan  -- time difference between LMT_AN and 
 %   Returns
-%       JD          -- Julian date to use with 337.5 ° RAAN to obtain the
+%       jd          -- Julian date to use with 337.5 ° RAAN to obtain the
 %                      10:30 am pass for the ascending node and the wanted
-%                       initial LOAN
+%                      initial LOAN
 
-% time of vernal equinix @ longitude of initial LOAN
-time_ve_loan = hours(initLOAN / 15);
-delta_t = hours(10.5) - time_ve_loan;
-% add 8 minutes (from the equation of time) to obtain the Local Apparent
-% Solar Time (LAT = LMT + ET)
-time_gw_raan = delta_t + minutes(8);
+ET = -7.5;
+LAT_AN = 10.5;
+LMT_AN = hours(LAT_AN) - minutes(ET);
 
-ve = datetime('20-March-2020') + time_gw_raan;
+dt_gw_loan = hours(init_LOAN / 15);
+dt_ve_loan = LMT_AN - dt_gw_loan;
+
+ve = datetime('20-March-2022') + dt_ve_loan;
 % Convert to Julian date
-jd = juliandate(ve);
+jd = juliandate(ve)
 end
+
 
